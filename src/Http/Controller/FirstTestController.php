da183d27d4be4f14c8c2af5e1fe8d114474402c6
Marshan Eric <marshansr@gmail.com>
2023-02-09 23:50:10 +0200

    add swagger

--- src/Http/Controller/FirstTestController.php
+++ src/Http/Controller/FirstTestController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controller;
 
 use App\Domain\Service\FirstTest\FirstTestService;
 use App\Http\Response\FirstTest\FirstTestIndexResponse;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +19,12 @@ class FirstTestController extends AbstractController
     ) {
     }
 
-    #[Route('/api/v1/first-test', methods: ['GET', 'HEAD'], name: 'api.first-test')]
+    #[OA\Response(
+        response: 200,
+        description: 'Get all FirstTest.',
+        content: new OA\JsonContent(ref: new Model(type: FirstTestIndexResponse::class)),
+    )]
+    #[Route('/api/v1/first-test', methods: ['GET'], name: 'api.first-test')]
     public function index(): Response
     {
         $firstTests = $this->firstTestService->findAll();
