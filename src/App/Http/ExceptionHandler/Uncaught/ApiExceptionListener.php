e19ab1bbdc516b3ba81b7e220fe027b743cfbed6
Marshan Eric <marshansr@gmail.com>
2023-03-01 15:52:14 +0200

    add isDev and isProd parameters to container

--- src/App/Http/ExceptionHandler/Uncaught/ApiExceptionListener.php
+++ src/App/Http/ExceptionHandler/Uncaught/ApiExceptionListener.php
@@ -7,18 +7,14 @@ namespace App\Http\ExceptionHandler\Uncaught;
 use App\Http\Response\Error\ErrorResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
-use Throwable;
 
 class ApiExceptionListener
 {
     public function __construct(
         private ExceptionResolver $exceptionResolver,
         private SerializerInterface $serializer,
-        private bool $isDev,
         private bool $isDebug
     ) {
     }
@@ -36,18 +32,6 @@ class ApiExceptionListener
         $message = $this->exceptionResolver->getMessage($throwable);
 
         $responseModel = new ErrorResponse($message);
-
-        if ($this->isDev) {
-            $responseModel = new ErrorResponse($message, [
-                '_debug' => [
-                    'message' => $throwable->getMessage(),
-                    'class' => $throwable::class,
-                    'file' => $throwable->getFile(),
-                    'line' => $throwable->getLine(),
-                    'trace' => $throwable->getTrace(),
-                ]
-            ]);
-        }
 
         $json = $this->serializer->serialize($responseModel, JsonEncoder::FORMAT);
         $response = new JsonResponse($json, $metadata->getHttpCode(), [], true);
